dc0e6c17df392bee4b67a9ffeeee61d51ff6e318
Tahsin Abrar <abrar.cste@gmail.com>
2014-10-15 13:26:47 +0600

    Layout file for the app

---
+++ app/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Learning Laravel App</title>
+</head>
+<body>
+  <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="./about">About</a></li>
+      <li><a href="./contact">Contact</a></li>
+      </ul>
+</body>
+</html>
