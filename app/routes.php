f25c21790027bf80af5991a81e0c960d355d9259
Tahsin Abrar <abrar.cste@gmail.com>
2014-10-15 15:03:06 +0600

    Adding validation in the contact page with laravel Validation

--- app/routes.php
+++ app/routes.php
@@ -23,3 +23,18 @@ Route::get('/about', function(){
 Route::get('/contact', function(){
     return View::make('contact');
 });
+
+Route::post('/contact', function(){
+    $data = Input::all();
+    $rules = array(
+        'subject' => 'required',
+        'message' => 'required'
+    );
+
+    $validator = Validator::make($data, $rules);
+
+    if($validator->fails()){
+        return Redirect::to('contact')->withErrors($validator)->withInput();
+    }
+    return 'Your message has been sent.';
+});
--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -4,6 +4,8 @@
 <h1>Contact</h1>
 <p>Please contact us by sending a message using the form below:</p>
 
+{{ HTML::ul($errors->all(),array('class'=>'errors')) }}
+
 {{ Form::open(array('url'=> 'contact')) }}
 {{ Form::label('Subject') }}
 {{ Form::text('subject', 'Enter your subject') }}
