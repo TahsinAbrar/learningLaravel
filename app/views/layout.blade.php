9b1473d447a9fa43bbfe3e1b943fa8f5f8a4d511
Tahsin Abrar <abrar.cste@gmail.com>
2014-10-15 13:28:10 +0600

    yield (cotent) added to the layout page

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -9,6 +9,7 @@
       <li><a href="/">Home</a></li>
       <li><a href="./about">About</a></li>
       <li><a href="./contact">Contact</a></li>
-      </ul>
+  </ul>
+  @yield('content')
 </body>
 </html>
