baa722721e422e93453545cadd0dba4e8aaf1939
Tahsin Abrar <abrar.cste@gmail.com>
2014-10-16 11:41:30 +0600

    Using offfset to align the content into center of the page

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -10,12 +10,20 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 </head>
 <body>
-  <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="about">About</a></li>
-      <li><a href="contact">Contact</a></li>
-  </ul>
-  @yield('content')
+    <div class="container col-md-8 col-md-offset-2">
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="navbar-header">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="/">Home</a></li>
+                    <li><a href="about">About</a></li>
+                    <li><a href="contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        @yield('content')
+
+    </div>
   {{--Adding jQuery CDN --}}
   <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
   <!-- Latest compiled and minified JavaScript -->
