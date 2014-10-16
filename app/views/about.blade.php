1ef33c482ca1b278616e9e011dcb587d33eaccba
Tahsin Abrar <abrar.cste@gmail.com>
2014-10-16 15:09:05 +0600

    Changing about and contact page to look good.

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -1,8 +1,24 @@
 @extends('layout')
 
 @section('content')
-<h1>About</h1>
-<p>Learning Laravel is a beginner to intermediate level book, designed for any P\
-   5 HP developer at all levels. The main goal of this book is to build a solid founda\
-   6 tion for developers to build their next web applications with Laravel</p>
+		<section class="header section-padding">
+			<div class="background">&nbsp;</div>
+			<div class="container">
+				<div class="header-text">
+					<h1>About Us</h1>
+					<p>
+					We're young and creative!
+					</p>
+				</div>
+			</div>
+		</section>
+
+        <div class="container">
+			<section class="section-padding">
+				<div class="jumbotron text-center">
+						<h1>ABOUT US</h1>
+						<p>Learning Laravel is a beginner to intermediate level book, designed for any PHP developer at all levels. The main goal of this book is to build a solid foundation for developers to build their next web applications with Laravel.</p>
+				</div>
+			</section>
+		</div>
 @stop
--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -1,18 +1,35 @@
 @extends('layout')
 
 @section('content')
-<h1>Contact</h1>
-<p>Please contact us by sending a message using the form below:</p>
+		<section class="header section-padding">
+			<div class="background">&nbsp;</div>
+			<div class="container">
+				<div class="header-text">
+					<h1>Contact Us</h1>
+					<p>
+					Feel free to contact us!
+					</p>
+				</div>
+			</div>
+		</section>
 
-{{ HTML::ul($errors->all(),array('class'=>'errors')) }}
+        <div class="container">
+			<section class="section-padding">
+				<div class="jumbotron text-center">
+                    <h1>HAVE ANY QUESTIONS?</h1>
+                    <p>Please contact us by sending a message using the form below:</p>
+                    {{ HTML::ul($errors->all(),array('class'=>'errors')) }}
 
-{{ Form::open(array('url'=> 'contact')) }}
-{{ Form::label('Subject') }}
-{{ Form::text('subject', 'Enter your subject') }}
-<br/>
-{{ Form::label('Message') }}
-{{ Form::textarea('message','Enter your message') }}
-<br/>
-{{ Form::submit() }}
-{{ Form::close() }}
+                    {{ Form::open(array('url'=> 'contact')) }}
+                    {{ Form::label('Subject') }}
+                    {{ Form::text('subject', 'Enter your subject') }}
+                    <br/>
+                    {{ Form::label('Message') }}
+                    {{ Form::textarea('message','Enter your message') }}
+                    <br/>
+                    {{ Form::submit() }}
+                    {{ Form::close() }}
+				</div>
+			</section>
+		</div>
 @stop
