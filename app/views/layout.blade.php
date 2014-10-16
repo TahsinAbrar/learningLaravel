6294903e179a5846bdd6b970a96f73cfb017b912
Tahsin Abrar <abrar.cste@gmail.com>
2014-10-16 12:26:39 +0600

    responsive navigation with bootstrap

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -11,20 +11,34 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 </head>
 <body>
-    <div class="container col-md-8 col-md-offset-2">
+    <header>
         <nav class="navbar navbar-default" role="navigation">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <ul class="nav navbar-nav">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Learning Laravel</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
                     <li class="active"><a href="/">Home</a></li>
                     <li><a href="about">About</a></li>
                     <li><a href="contact">Contact</a></li>
-                </ul>
-            </div>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
         </nav>
+    </header>
 
-        @yield('content')
+    @yield('content')
 
-    </div>
   {{--Adding jQuery CDN --}}
   <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
   <!-- Latest compiled and minified JavaScript -->
